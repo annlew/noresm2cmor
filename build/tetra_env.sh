92c566bbac26fbfb46665f0585d2f978241ae118
annlew <anna@misu.su.se>
2022-11-11 10:51:06 +0100

    add makefile and env for Tetralith/NSC

---
+++ build/tetra_env.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+module use /proj/bolinc/shared/software/modules/
+module load buildenv-intel/2018.u1-bare 
+module load netCDF/4.3.2-HDF5-1.8.12-nsc1-intel-2018.u1-bare
+module load cmor/3.7.0-intel-2018.u1-bare
+module load UDUNITS2/2.2.26-nsc1-gcc-7.3.0-bare
+module load json-c/0.14-intel-2018.u1-bare
